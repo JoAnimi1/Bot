e70341a18f43080c0d93e8b96614fd5129c225b4
Diego Flores <diego.flores@cafirexos.com>
2024-03-18 17:22:07 -0600

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -1,5 +1,4 @@
-const axios = require('axios');
-const fs = require('fs');
+import axios from 'axios';
 
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
